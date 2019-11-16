78b4aca6a7a6c88e0148cf2f94a5a7e909d2a190
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-11-16 15:13:02 -0700

    Initial structure

--- Controllers/NeedlesController.cs
+++ Controllers/NeedlesController.cs
@@ -8,35 +8,35 @@ namespace needle_tracker.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class NeedlesController : ControllerBase
     {
-        // GET api/values
+        // GET api/needles
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "needle1", "needle2" };
         }
 
-        // GET api/values/5
+        // GET api/needles/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return "needle";
         }
 
-        // POST api/values
+        // POST api/needles
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] string needle)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/needles/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] string needle)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/needles/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
---
+++ Models/Needle.cs
@@ -0,0 +1,4 @@
+namespace needle_tracker.Models
+{
+    
+}
