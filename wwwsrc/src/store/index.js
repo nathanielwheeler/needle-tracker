9ab15f07dd632428b21ee3be8863b0c5bd5f336c
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-11-19 15:23:27 -0700

    client store

--- wwwsrc/src/store/index.js
+++ wwwsrc/src/store/index.js
@@ -1,14 +1,70 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
+import router from './router'
+import AuthService from '../services/AuthService'
+import { generateKeyPairSync } from 'crypto'
 
 Vue.use(Vuex)
 
+let baseUrl = location.host.includes('localhost') ? 'https://localhost:5001' : '/'
+
+let api = Axios.create({
+  baseURL: baseUrl + "api/",
+  timeout: 3000,
+  withCredentials: true
+})
+
 export default new Vuex.Store({
   state: {
+    user: {},
+    entries: [],
   },
   mutations: {
+    setUser(state, user) {
+      state.user = user
+    },
+    resetState(state) {
+      //clear state object of user data
+      state.user = {}
+    },
+    setEntries(state, entries) {
+      state.entries = entries
+    }
   },
   actions: {
+    // #region Auth
+
+    async register({ commit, dispatch }, creds) {
+      try {
+        let user = await AuthService.Register(creds)
+        commit('setUser', user)
+        router.push({ name: "dashboard" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
+    async login({ commit, dispatch }, creds) {
+      try {
+        let user = await AuthService.Login(creds)
+        commit('setUser', user)
+        router.push({ name: "dashboard" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
+    async logout({ commit, dispatch }) {
+      try {
+        let success = await AuthService.Logout()
+        if (!success) { }
+        commit('resetState')
+        router.push({ name: "login" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
+
+    // #endregion
   },
   modules: {
   }
